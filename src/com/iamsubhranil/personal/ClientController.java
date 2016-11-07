6b2bf89da731349d121cbc471e9c2248b442ce0d
iamsubhranil <subhranilmukherjee190@gmail.com>
2016-11-07 16:21:45 +0530

    Added basic client UI

---
+++ src/com/iamsubhranil/personal/ClientController.java
@@ -0,0 +1,24 @@
+package com.iamsubhranil.personal;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * Author : Nil
+ * Date : 11/7/2016 at 4:14 PM.
+ * Project : ClientServerBasics
+ */
+public class ClientController {
+    public Label localAddressLabel;
+    public Label localPortLabel;
+    public Label remoteAddressLabel;
+    public Label remotePortLabel;
+    public Label statusLabel;
+    public TextArea terminalField;
+    public TextField commandField;
+
+    public void sendCommandAndErase(ActionEvent actionEvent) {
+    }
+}
